d06ee19e5e573cbaf12f9ee20f12611383e40d16
takuma <takuma.y0524@gmail.com>
2020-11-23 01:09:12 +0900

    Fix type definitions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,50 +1,59 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 
 type BoxId = string;
-type BoxType = "wallet" | "card";
 
 type Box = {
-  id: BoxId,
-  name: string,
-  type: BoxType,
-  amount: number,
+  id: BoxId;
+  name: string;
+  amount: number;
 };
 
 
 type TransactionCategoryId = string;
 
 type TransactionCategory = {
-  id: TransactionCategoryId,
-  name: string,
+  id: TransactionCategoryId;
+  name: string;
 };
 
+
 type TransactionEntry = {
-  createdAt: Date,
-  updatedAt: Date,
-  amount: number,
-  category: TransactionCategoryId,
-  from?: BoxId,
-  to?: BoxId,
+  createdAt: Date;
+  updatedAt: Date;
+  amount: number;
+  category: TransactionCategoryId;
+  from?: BoxId;
+  to?: BoxId;
 };
 
 
 type AppData = {
-  entries: TransactionEntry[],
-}
+  entries: TransactionEntry[];
+  categories: TransactionCategory[];
+  boxes: Box[];
+};
 
 
 export default new Vuex.Store<AppData>({
   state: {
-    entries: []
+    entries: [
+      { createdAt: new Date(), updatedAt: new Date(), amount: 1000, category: "c4821", from: "b8748" },
+    ],
+    categories: [
+      { id: "c4821", name: "食費" },
+    ],
+    boxes: [
+      { id: "b8748", name: "財布", amount: 30000 },
+    ],
   },
   mutations: {
   },
   actions: {
   },
   modules: {
-  }
+  },
 });
